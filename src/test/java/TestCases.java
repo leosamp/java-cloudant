9577fe4df52e6b991aad5a805ccad538876df4de
leosamp <leo.sampaio@gmail.com>
2018-05-01 15:02:01 -0300

    Added list of test cases -  TestCases.java

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCases {
+	
+	private	String MAVEN_HOME;
+	
+	@Before
+	public void setUp() throws Exception {
+		MAVEN_HOME = System.getenv("MAVEN_HOME");
+  }
+
+  @Test
+	public void testMavenHome() {
+		Assert.assertNotNull("Apache Maven is not setup!", MAVEN_HOME);
+	}
+}
