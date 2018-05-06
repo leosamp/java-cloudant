db550fc549b8d0c61ba1a39affdd50fd5085922d
leosamp <leo.sampaio@gmail.com>
2018-05-06 12:31:21 -0300

    Create TestCases2.java

---
+++ src/test/java/TestCases2.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import com.cloudant.client.api.Database;
+
+public class TestCases {
+   
+   private String MAVEN_HOME;
+   private Database db = null;
+
+   @Before
+   public void setUp() throws Exception {
+      MAVEN_HOME = System.getenv("MAVEN_HOME");
+      try{
+      	db = CloudantClientMgr.getDB();
+      } catch (Exception e) {
+      	return;
+      }
+   }
+
+   @Test
+   public void testMavenHome() {
+      Assert.assertNotNull("Apache Maven is not setup!", MAVEN_HOME);
+   }
+   
+   @Test
+   public void testDatabaseConnection(){
+   	  Assert.assertNotNull("Database connection failed!",db);
+   }
+	
+}
